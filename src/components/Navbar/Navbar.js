7a25dec7e062a8b1dd13508d7e00d742c5f44d7b
Fabian Phillipe <fabianphillipe@gmail.com>
2021-10-20 17:36:21 +0200

    navbar almost ready

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './NavBar.scss';
 
 import { FaBars } from 'react-icons/fa';
@@ -6,17 +6,32 @@ import { DiReact } from 'react-icons/di';
 import { links, social } from './navdata';
 
 function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav__center">
         <div className="nav__header">
           <DiReact className="logo" />
-          <button className="nav__toggle">
+          <button className="nav__toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
-        <div className="links__container">
-          <ul className="links">
+        <div className="links__container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map(({ id, url, text }) => {
               return (
                 <li key={id}>
